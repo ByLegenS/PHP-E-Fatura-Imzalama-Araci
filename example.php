5bcd6452e68b88ced54b6303b6e84ccfe4373a98
muratkaragoz <muratkaragoz@users.noreply.github.com>
2018-10-26 21:46:38 +0300

    Update example.php

--- example.php
+++ example.php
@@ -1,36 +1,39 @@
 <?php 
 
-	/* Smart Kart Fatura İmzalama */
-	function Imzala($java_dosyasi, $fatura_dosyasi, $pin=null, $seriImza=false, $dosyayaKaydet=true)
+	/* Smart Kart Fatura İmzalama */  
+	function Imzala($java_dosyasi,$fatura_dosyasi,$pin=null,$seriImza=false,$dosyayaKaydet=true)  
+	{  
+	/* Java Dosyasına Veri Yollanıyor */  
+	if($pin!=null)  { 
+	exec("java -Dfile.encoding=UTF8 -jar ".$java_dosyasi." {".$fatura_dosyasi.",".$pin.",".$seriImza.",".$dosyayaKaydet."} 2>&1",$cikti);    
+	$sonuc=$cikti;  
+
+	print_r($cikti);
+	
+	foreach($cikti AS $ciktiDeger)  
+	{  
+	
+		if(strstr($ciktiDeger,'İmzalandı...')==true)  
+			{  
+				$sonuc=true;  break;  
+				}  
+	}   
+	return $sonuc;  
+	}  
+	}  
+	
+	
+	/* Smart Kart Fatura İmzalama */   $dizin=(__DIR__ . DIRECTORY_SEPARATOR);  $java_dosyasi=$dizin."Java/NTGEFaturaImza.jar";  
+	$fatura_dosyasi='TemelFaturaOrnegi.xml';  
+	$pin='123456';   
+	$cikti=Imzala($java_dosyasi,$fatura_dosyasi,$pin);   
+	
+	if($cikti==true)
 	{
-		/* Java Dosyasına Veri Yollanıyor */
-		if($pin != null)
-		{	
-			exec("java -Dfile.encoding=UTF8 -jar $java_dosyasi {$fatura_dosyasi,$pin,$seriImza,$dosyayaKaydet} 2>&1", $cikti);
-			
-			$sonuc = $cikti;
-			
-			foreach ($cikti AS $ciktiDeger)
-			{
-				if(strstr($ciktiDeger, 'İmzalandı...') == true)
-				{
-					$sonuc = true;
-					break;
-				}
-			}
-			
-			return $sonuc;
-		}
+		echo "İmzalandı..."; 
+	}		
+	else 
+	{
+		echo "Hata Oluştu...";
 	}
-	/* Smart Kart Fatura İmzalama */
-
-	$dizin = (__DIR__ . DIRECTORY_SEPARATOR);
-	$java_dosyasi=$dizin."Java/NTGEFaturaImza.jar";
-	$fatura_dosyasi='TemelFaturaOrnegi.xml';
-	$pin='123456';
-
-	$cikti = Imzala($java_dosyasi, $fatura_dosyasi, $pin);
-
-	if($cikti == true) echo 'İmzalandı...';
-	else echo 'Hata Oluştu...';
 ?>
