72766df87da22e6afc75f5cf41fb3feb66b14d89
muratkaragoz <muratkaragoz@users.noreply.github.com>
2018-11-04 19:56:57 +0300

    Update and rename example.php to index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+
+	/* Smart Kart Fatura İmzalama */   
+	$dizin=(__DIR__ . DIRECTORY_SEPARATOR);  
+	$java_dosyasi=$dizin."Java\NTGEFaturaImza.jar";  
+	$fatura_dosyasi=$dizin."TemelFaturaOrnegi.xml";  
+	$pin='123456';  
+	
+	
+
+	
+	
+	/* Smart Kart Fatura İmzalama */  
+	function Imzala($java_dosyasi,$fatura_dosyasi,$pin=null,$seriImza=false,$dosyayaKaydet=true)  
+	{  
+	
+		/* Java 0 - 1 i kabul etmiyor. İlla True - False olacak diyor */ 
+	if ($seriImza== 0) { $seriImza= "false"; } elseif ($seriImza== 1) { $seriImza= "true"; }
+	if ($dosyayaKaydet== 0) { $dosyayaKaydet= "false"; } elseif ($dosyayaKaydet== 1) { $dosyayaKaydet= "true"; }
+	
+	
+		/* Java Dosyasına Veri Yollanıyor */  
+		if($pin!=null)  
+		{ 
+				$java_komutu= $java_dosyasi." {".$fatura_dosyasi.",".$pin.",".$seriImza.",".$dosyayaKaydet."}";
+				exec("java -Dfile.encoding=UTF8 -jar ".$java_komutu." 2>&1",$cikti);    
+				$sonuc=$cikti;  
+
+				
+				foreach($cikti AS $ciktiDeger)  
+				{  
+	
+						if(strstr($ciktiDeger,'İmzalandı...')==true)  
+						{  
+								$sonuc=true;  break;  
+						}  
+				}   
+			return $sonuc;  
+		}  
+	}  
+	
+	
+ 
+	$cikti=Imzala($java_dosyasi,$fatura_dosyasi,$pin,false,true);   
+	
+	if($cikti==true)
+	{
+		echo "İmzalandı..."; 
+	}		
+	else 
+	{
+		echo "Hata Oluştu...";
+	}
+?>
